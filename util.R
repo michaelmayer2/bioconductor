0e8bfd7c3c51c4b2dbd62f15d01dd6805163e902
Michael Mayer <michael.mayer@rstudio.com>
2022-02-17 10:45:30 +0000

    remove env variables

--- util.R
+++ util.R
@@ -3,19 +3,12 @@ system("rm -rf ~/R ~/.cache ~/.config ~/.Rprofile .Rprofile rsconnect renv")
 rstudioapi::restartSession()
 }
 
-envset <- function() {
-  Sys.setenv("CONNECT_USER"="mmayer")
-  Sys.setenv("CONNECT_USER_KEY"="SGlhHT0KQ1Qvc4JRNIlbpv3kcoZCdSjW")
-  Sys.setenv("CONNECT_SERVER"="http://192.168.0.100:3939")
-  
-}
-
 connectsetup <- function() {
   rsconnect::addConnectServer(Sys.getenv("CONNECT_SERVER"), name="Connect")
   rsconnect::connectApiUser(account=Sys.getenv("CONNECT_USER"),apiKey = Sys.getenv("CONNECT_USER_KEY"))
 }
 
-envset()
+
 connectsetup()
 
 rsconnect::deployApp(appDir="shiny")
